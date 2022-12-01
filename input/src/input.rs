d0206acabdf01f7fc9b6077d47bebc2a9d8ca85b
Tobias Prisching <tobias.prisching@icloud.com>
2022-12-01 07:17:34 +0100

    Added function for reading file into multiple vectors

--- input/src/input.rs
+++ input/src/input.rs
@@ -29,7 +29,37 @@ pub fn read_data
 	return Ok(data);
 }
 
+pub fn read_vecs
+<T: FromStr + std::default::Default + std::fmt::Display + std::clone::Clone>
+(
+	path: &std::path::Path,
+)
+-> Result<Vec<Vec<T>>, Box<dyn std::error::Error>>
+{	
+	let file = File::open(path)?;
+	let lines = BufReader::new(file).lines();
+	let mut data = Vec::<Vec::<T>>::new();
 
+	let mut temp = Vec::<T>::new();
+
+	for result_line in lines
+	{
+		let line = result_line?;
+
+		if line == ""
+		{
+			data.push(temp.clone());
+			temp.clear();
+		}
+		else
+		{
+			temp.push(T::from_str(&line).unwrap_or_default());
+		}
+
+	}	
+
+	return Ok(data);
+}
 
 pub fn read_string_data
 (
