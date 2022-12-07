6fef94f6d463432742518f84aaa70607f30dded2
Tobias Prisching <tobias.prisching@icloud.com>
2022-12-07 20:21:03 +0100

    Update main.rs

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,53 +1,12 @@
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::str::FromStr;
 use std::collections::HashMap;
 
-trait filesys_object
-{
-	fn get_name(&self) -> String;
-	fn get_size(&self) -> u64;
-}
-
-struct directory
-{
-	name: String,
-	children: Vec<Box<dyn filesys_object>>
-}
-
-struct file
+struct ElfFile
 {
 	name: String,
 	size: u64
-}
-
-impl filesys_object
-for directory
-{
-	fn get_name(&self) -> String
-	{
-		self.name.clone()
-	}
-
-	fn get_size(&self) -> u64
-	{
-		self.children.iter().map(|x| x.get_size()).sum()
-	}
-}
-
-impl filesys_object
-for file
-{
-	fn get_name(&self) -> String
-	{
-		self.name.clone()
-	}
-
-	fn get_size(&self) -> u64
-	{
-		self.size
-	}
 }
 
 fn read_string_data
@@ -76,15 +35,8 @@ fn parse_string_data
 // -> Box<dyn filesys_object>
 {
 
-	let mut directories = HashMap::new();
-	directories.insert(
-		"/".to_string(),
-		Vec::<file>::new()
-	);
-
-	// let mut root_directory = directory {name: "/".to_string(), children: Vec::new()};
-
-	let mut current_path = "".to_string();
+	let mut directories = HashMap::from([("/".to_string(), Vec::<ElfFile>::new())]);
+	let mut current_path = "/".to_string();
 
 	for line in data
 	{
@@ -115,9 +67,7 @@ fn parse_string_data
 							},
 						}
 					},
-					Some('l') => { // List...
-						
-					},
+					Some('l') => (),
 					_ => panic!("AH")
 				}
 			},
@@ -125,75 +75,74 @@ fn parse_string_data
 				let directory_name = format!("{}{}/", current_path, &line[4..]);
 				directories.insert(
 					directory_name,
-					Vec::<file>::new()
+					Vec::<ElfFile>::new()
 				);
 			},
-			Some(digit) => { // File
-				let mut line_parts = line.split(' ').map(|x| x.to_string()).collect::<Vec<String>>();
-				let file_size = line_parts[0].parse::<u64>().unwrap();
+			Some(_) => { // File
+				let mut line_parts = line
+					.split(' ')
+					.map(|x| x.to_string())
+					.collect::<Vec<String>>();
+				let file_size = line_parts[0].parse::<u64>().unwrap_or_default();
 				line_parts.remove(0);
 				let file_name = line_parts.join(" ");
-				println!("Current path: {}", current_path);
-				println!("{}", file_name);
-				println!("{}", file_size);
-				directories.get_mut(&current_path).unwrap().push(file {name: file_name, size: file_size});
+				directories.get_mut(&current_path).unwrap().push(
+					ElfFile {name: file_name, size: file_size}
+				);
 			}
 			_ => panic!("AH")
 		}
 	}
 
-	let mut dir_sizes = HashMap::new();
+	let mut directory_sizes = Vec::new();
 	for entry in &directories
 	{
-		let mut subdir_size = 0u64;
-		for e in &directories
+		let mut directory_size = 0u64;
+		for other_entry in &directories
 		{
-			if e.0.contains(entry.0) && e.0 != entry.0
+			if 
+			(other_entry.0.contains(entry.0)) &&                                // other_entry is an entry for a subdirectory
+			(other_entry.0 != entry.0)                                          // ensuring that they are not the same directory
 			{
-				for f in e.1
-				{
-					subdir_size += f.size;
-				}
+				directory_size += other_entry.1
+					.iter()
+					.map(|file| file.size)
+					.sum::<u64>();
 			}
 		}
 
-		let mut dir_size = subdir_size;
-		for f in entry.1
-		{
-			dir_size += f.size;
-		}
-		dir_sizes.insert(entry.0.clone(), dir_size);
-		println!("{}: {}", entry.0, dir_size);
+		directory_size += entry.1
+					.iter()
+					.map(|file| file.size)
+					.sum::<u64>();
+
+		directory_sizes.push(directory_size);
+		println!("{}: {}", entry.0, directory_size);
 	}
 
-	println!("Final sum for part 1: {}", dir_sizes.iter().map(|x| x.1).filter(|&x| *x < 100000u64).sum::<u64>());
+	println!("Final sum for part 1: {}", directory_sizes.iter().filter(|&x| *x < 100000u64).sum::<u64>());
 
 	let disk_size = 70000000u64;
 	let space_required = 30000000u64;
 
-	let mut used_space = 0u64;
-	for e in &directories
-	{
-		for f in e.1
-		{
-			used_space += f.size;
-		}
-	}
+	let used_space = directories
+		.iter()
+		.map(
+			|directory| directory.1
+				.iter()
+				.map(|file| file.size)
+				.sum::<u64>()
+		)
+		.sum::<u64>();
 
 	let needs_to_be_freed = space_required - (disk_size-used_space);
-
-	let mut current_smallest_viable_dir = "/".to_string();
-	let mut current_smallest_viable_size = disk_size;
-	for dir in dir_sizes
-	{
-		if dir.1 >= needs_to_be_freed && dir.1 < current_smallest_viable_size
-		{
-			current_smallest_viable_size = dir.1;
-			current_smallest_viable_dir = dir.0.clone();
-		}
-	}
-
-	println!("{}", current_smallest_viable_size);
+	let smallest_viable_directory_size = directory_sizes
+		.iter()
+		.filter(|size| *size >= &needs_to_be_freed)
+		.min()
+		.unwrap_or(&used_space);
+	
+	println!("Final size for part 2: {}", smallest_viable_directory_size);
 
 }
 
