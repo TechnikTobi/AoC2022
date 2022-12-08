287c67447901800e6c637629fe53a4b2bef41a56
Tobias Prisching <tobias.prisching@icloud.com>
2022-12-08 08:46:56 +0100

    Fixed indentation

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -18,33 +18,33 @@ pub fn read_line
 		return Ok(result_line?);
 	}
 
-    return Ok("".to_string());
+	return Ok("".to_string());
 }
 
 fn main() {
-    
-    let mut line = read_line(
-        std::path::Path::new("./data/input.txt")
-    ).unwrap().chars().collect::<VecDeque<_>>();
+	
+	let mut line = read_line(
+		std::path::Path::new("./data/input.txt")
+	).unwrap().chars().collect::<VecDeque<_>>();
 
-    // Part 1 & 2
-    let marker_len = 14; // 4; for part 1
-    let mut marker = VecDeque::<char>::new();
+	// Part 1 & 2
+	let marker_len = 14; // 4; for part 1
+	let mut marker = VecDeque::<char>::new();
 
-    for marker_index in 1..(line.len()+1)
-    {
-        marker.push_back(line.pop_front().unwrap());
+	for marker_index in 1..(line.len()+1)
+	{
+		marker.push_back(line.pop_front().unwrap());
 
-        while marker.len() > marker_len
-        {
-            marker.pop_front();
-        }
+		while marker.len() > marker_len
+		{
+			marker.pop_front();
+		}
 
-        if marker.iter().map(|&x| x).collect::<HashSet<char>>().len() == marker_len
-        {
-            println!("Maker position: {}", marker_index);
-            return;
-        }
-    }
+		if marker.iter().map(|&x| x).collect::<HashSet<char>>().len() == marker_len
+		{
+			println!("Maker position: {}", marker_index);
+			return;
+		}
+	}
 
 }
