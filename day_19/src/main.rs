e2917062697c77bb4f23199930d30a3a3a3d54b1
Tobias Prisching <tobias.prisching@icloud.com>
2022-12-22 19:50:59 +0100

    Update main.rs

--- day_19/src/main.rs
+++ day_19/src/main.rs
@@ -21,71 +21,82 @@ pub fn read_string_data
 	return Ok(data);	
 }
 
+fn
+parse_blueprint
+(
+	raw_blueprint: &String
+)
+-> Vec<Vec<i64>>
+{
+	let mut ore_robot_costs: Vec::<i64> = Vec::new();
+	let mut clay_robot_costs: Vec::<i64> = Vec::new();
+	let mut obsidian_robot_costs: Vec::<i64> = Vec::new();
+	let mut geode_robot_costs: Vec::<i64> = Vec::new();
+
+	for (index, costs) in raw_blueprint.split("costs").enumerate()
+	{
+		println!("{}", costs.split(".").next().unwrap());
+		match index
+		{
+			1 => {
+				ore_robot_costs = vec![
+					costs.split(".").next().unwrap().trim().split(" ").next().unwrap().parse::<i64>().unwrap(), 
+					0, 
+					0,
+					0
+				];
+			},
+			2 => {
+				clay_robot_costs = vec![
+					costs.split(".").next().unwrap().trim().split(" ").next().unwrap().parse::<i64>().unwrap(), 
+					0, 
+					0,
+					0
+				];
+			},
+			3 => {
+				obsidian_robot_costs = vec![
+					costs.split(".").next().unwrap().trim().split(" ").next().unwrap().parse::<i64>().unwrap(),
+					costs.split(".").next().unwrap().trim().split("and ").last().unwrap().split("clay").next().unwrap().trim().parse::<i64>().unwrap(),
+					0,
+					0
+				];
+			},
+			4 => {
+				geode_robot_costs = vec![
+					costs.split(".").next().unwrap().trim().split(" ").next().unwrap().parse::<i64>().unwrap(),
+					0,
+					costs.split(".").next().unwrap().trim().split("and ").last().unwrap().split("obsidian").next().unwrap().trim().parse::<i64>().unwrap(),
+					0
+				];
+			},
+			_ => ()
+		}	
+	}
+
+	return vec![ore_robot_costs, clay_robot_costs, obsidian_robot_costs, geode_robot_costs];	
+}
+
 fn main() 
 {
 	let lines = read_string_data(
 		std::path::Path::new("./data/input.txt")
 	).unwrap();
 
-	let max_minutes = 24;
+	let max_minutes_part_1 = 24;
+	let max_minutes_part_2 = 32;
 	let mut quality_level_sum = 0;
 
 	for line in &lines
 	{
 		let blueprint_id = line[10..].split(":").next().unwrap().parse::<i64>().unwrap();
 
-		let mut ore_robot_costs: Vec::<i64> = Vec::new();
-		let mut clay_robot_costs: Vec::<i64> = Vec::new();
-		let mut obsidian_robot_costs: Vec::<i64> = Vec::new();
-		let mut geode_robot_costs: Vec::<i64> = Vec::new();
-
-		for (index, costs) in line.split("costs").enumerate()
-		{
-			println!("{}", costs.split(".").next().unwrap());
-			match index
-			{
-				1 => {
-					ore_robot_costs = vec![
-						costs.split(".").next().unwrap().trim().split(" ").next().unwrap().parse::<i64>().unwrap(), 
-						0, 
-						0,
-						0
-					];
-				},
-				2 => {
-					clay_robot_costs = vec![
-						costs.split(".").next().unwrap().trim().split(" ").next().unwrap().parse::<i64>().unwrap(), 
-						0, 
-						0,
-						0
-					];
-				},
-				3 => {
-					obsidian_robot_costs = vec![
-						costs.split(".").next().unwrap().trim().split(" ").next().unwrap().parse::<i64>().unwrap(),
-						costs.split(".").next().unwrap().trim().split("and ").last().unwrap().split("clay").next().unwrap().trim().parse::<i64>().unwrap(),
-						0,
-						0
-					];
-				},
-				4 => {
-					geode_robot_costs = vec![
-						costs.split(".").next().unwrap().trim().split(" ").next().unwrap().parse::<i64>().unwrap(),
-						0,
-						costs.split(".").next().unwrap().trim().split("and ").last().unwrap().split("obsidian").next().unwrap().trim().parse::<i64>().unwrap(),
-						0
-					];
-				},
-				_ => ()
-			}	
-		}
-
-		let costs = vec![ore_robot_costs, clay_robot_costs, obsidian_robot_costs, geode_robot_costs];
+		let costs = parse_blueprint(&line);
 		let mut robots = vec![0i64, 0, 0, 0];
 		let mut resources = vec![costs[0][0].clone(), 0, 0, 0];
 
-		// Get result and insert it
-		let blueprint_state = ProblemState { costs, robots, next_robot_index: 0, resources, minute: 0, max_minutes: max_minutes }; 
+		// Get result
+		let blueprint_state = ProblemState { costs, robots, next_robot_index: 0, resources, minute: 0, max_minutes: max_minutes_part_1 }; 
 		let blueprint_result = part_1(blueprint_state);
 		println!("Result for blueprint {}: {}", blueprint_id, blueprint_result);
 		
@@ -93,7 +104,31 @@ fn main()
 	}
 
 	let part_1_result = quality_level_sum;
-	println!("Part 1: {}", part_1_result);
+	println!("Part 1: {}\n", part_1_result);
+
+
+	let mut part_2_result = 1;
+	for (line_index, line) in lines.iter().enumerate()
+	{
+		if line_index > 2
+		{
+			break;
+		}
+
+		let blueprint_id = line[10..].split(":").next().unwrap().parse::<i64>().unwrap();
+
+		let costs = parse_blueprint(&line);
+		let mut robots = vec![0i64, 0, 0, 0];
+		let mut resources = vec![costs[0][0].clone(), 0, 0, 0];
+
+		// Get result
+		let blueprint_state = ProblemState { costs, robots, next_robot_index: 0, resources, minute: 0, max_minutes: max_minutes_part_2 }; 
+		let blueprint_result = part_1(blueprint_state);
+		println!("Result for blueprint {}: {}", blueprint_id, blueprint_result);
+		part_2_result *= blueprint_result;
+	}
+
+	println!("Part 2: {}", part_2_result);
 	
 }
 
