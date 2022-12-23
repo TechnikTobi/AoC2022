b842467e1146f625c25ef956a2f5af72eaff38e7
Tobias Prisching <tobias.prisching@icloud.com>
2022-12-23 02:12:40 +0100

    Update main.rs

--- day_21/src/main.rs
+++ day_21/src/main.rs
@@ -1,9 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::hash::Hash;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::str::FromStr;
 
 pub fn read_string_data
 (
@@ -24,27 +22,6 @@ pub fn read_string_data
 	return Ok(data);	
 }
 
-// struct
-// Expression
-// {
-// 	expression_string: String
-// }
-
-// impl
-// Expression
-// {
-// 	fn 
-// 	evaluate
-// 	(
-// 		&self
-// 	)
-// 	-> f64
-// 	{
-// 		if self.expression_string
-// 		return 0.0;
-// 	}
-// }
-
 fn main() 
 {
 	let lines = read_string_data(
@@ -64,7 +41,10 @@ fn main()
 	println!("Part 1: {}", part_1_result);
 
 	let part_2_equation = evaluate_part_2(&monkey_expressions, &String::from("root"));
-	println!("Part 2: {}", part_2_equation);
+	println!("Part 2 Equation: {}", part_2_equation);
+
+	let part_2_result = resolve_part_2(&part_2_equation);
+	println!("Part 2: {}", part_2_result);
 
 }
 
@@ -125,7 +105,7 @@ evaluate_part_2
 
 	if monkey_to_evaluate == &String::from("humn")
 	{
-		return String::from("x");
+		return String::from("(x)");
 	}
 
 	let expression = monkey_expressions[monkey_to_evaluate].clone();
@@ -179,4 +159,86 @@ evaluate_part_2
 	{
 		return expression;
 	}
+}
+
+fn 
+resolve_part_2
+(
+	equation: &String
+) 
+-> f64
+{
+
+	let mut left_side = equation.split('=').nth(0).unwrap().trim();
+	let mut right_side = equation.split('=').nth(1).unwrap().trim();
+
+	// Make sure that x is on the left side
+	// Just a convention of mine
+	if right_side.contains('x')
+	{
+		let temp = left_side;
+		left_side = right_side;
+		right_side = temp;
+	}
+
+	// Parse the value on the right
+	let right_side_value = right_side.trim().parse::<f64>().unwrap();
+
+	// Remove parentheses
+	let mut left_expression = &left_side[1..left_side.len()-1];
+	let mut raw_operand = String::new();
+	let operator: char;
+
+	if left_expression.chars().next().unwrap() == '('
+	{
+		// Numeric operand is on the right
+		while left_expression.chars().last().unwrap().is_ascii_digit()
+		{
+			raw_operand.push(left_expression.chars().last().unwrap());
+			left_expression = &left_expression[..left_expression.len()-1];
+		}
+
+		operator = left_expression.chars().last().unwrap();
+		left_expression = &left_expression[..left_expression.len()-1];
+
+	}
+	else if left_expression.chars().last().unwrap() == ')'
+	{
+		// Numeric operand is on the left
+		while left_expression.chars().next().unwrap().is_ascii_digit()
+		{
+			raw_operand.push(left_expression.chars().next().unwrap());
+			left_expression = &left_expression[1..];
+		}
+		operator = left_expression.chars().next().unwrap();
+		left_expression = &left_expression[1..];
+	}
+	else
+	{
+		// Expression of form "(x)"
+		return right_side_value;
+	}
+
+	let left_side_operand = raw_operand.parse::<f64>().unwrap();
+
+
+
+	let new_right_side_value = match operator
+	{
+		'+' => right_side_value - left_side_operand,
+		'-' => right_side_value + left_side_operand,
+		'*' => right_side_value / left_side_operand,
+		'/' => right_side_value * left_side_operand,
+		_ => panic!("AH"),
+	};
+
+	let new_equation = format!("{} = {}", left_expression, new_right_side_value);
+
+	// println!("Input: {}", equation);
+	// println!("left: {}", left_expression);
+	// println!("right: {}", right_side);
+	// println!("New: {}", new_equation);
+	// println!("\n");
+
+	return resolve_part_2(&new_equation);
 }
